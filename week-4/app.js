d4c43722f1f8abbb76890b5d91a2c9cce0cfd6fc
ediiotero <e.otero81@gmail.com>
2024-10-31 22:16:37 -0400

    week 5 ready

---
+++ week-4/app.js
@@ -0,0 +1,29 @@
+const name = "Eddie"
+const last = "Otero"
+const isHappy = true
+const list = ['Tobi', 4, false]
+const obj = { key: 'value', allowAttachment: false}
+const nothing = null
+let test;
+
+console.log('name:',name + " " + last)
+console.log('isHappy:', isHappy)
+console.log('List:', list)
+console.log('Object:', obj)
+console.log('Nothing:', nothing)
+console.log('test:', test)
+
+const number = 5
+const number1 = '5'
+const number2 = 2
+
+console.log('test 1:', number == number1)
+console.log('test 2:', number === number1)
+
+console.log((number + number2) - 3)
+console.log(number - number2)
+console.log(number / number2)
+console.log(number * number2)
+console.log(number % number2)
+console.log(number > number2)
+console.log(number < number2)
